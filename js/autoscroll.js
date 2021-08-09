e98101baa557b5134ac99403982ada463706b19e
maxhorowitz <maxhorowitz@gmail.com>
2021-08-08 23:51:20 -0400

    progress

--- js/autoscroll.js
+++ js/autoscroll.js
@@ -20,13 +20,13 @@
 //     });
 // });
 
-// $(document).on('click', 'a[href^="#"]', function (event) {
-//     event.preventDefault();
-//     $('html, body, div, nav').animate(
-//         {scrollTop: $($.attr(this, 'href')).offset.top}, 
-//         500
-//     );
-// });
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $(document).animate(
+        {scrollTop: $($.attr(this, 'href')).offset.top}, 
+        500
+    );
+});
 
 // function smoothScrollingTo(target){
 //     $('html,body').animate({scrollTop:$(target).offset().​top}, 500);

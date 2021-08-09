8bc13453e7119cdc90be62005caef49a9fcf9bf3
maxhorowitz <maxhorowitz@gmail.com>
2021-08-09 19:25:37 -0400

    scroll should be working

--- js/autoscroll.js
+++ js/autoscroll.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('a[href*=\\#]:not([href=\\#])').on('click', function() {
+    $('a[href*="#"]:not([href="#"])').on('click', function() {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
         if (target.length) {

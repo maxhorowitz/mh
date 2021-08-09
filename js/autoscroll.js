b0698b401d10d80ccf84357acce46fead17a04d1
maxhorowitz <maxhorowitz@gmail.com>
2021-08-09 00:14:23 -0400

    auto scroll working

--- js/autoscroll.js
+++ js/autoscroll.js
@@ -1,40 +1,10 @@
-// $(function() {
-//     $('a[href*=\\#]:not([href=\\#])').on('click', function() {
-//         var target = $(this.hash);
-//         target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
-//         if (target.length) {
-//             $('html,body').animate({scrollTop: target.offset().top}, 1500);
-//             return false;
-//         }
-//     });
-// });
-
-// $(function() {
-//     $('a[href*=\\#]:not([href=\\#])').on('click', function() {
-//         var target = $(this.hash);
-//         target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
-//         if (target.length) {
-//             $('html,body').animate({scrollTop: target.offset().top}, 1500);
-//             return false;
-//         }
-//     });
-// });
-
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
-    $(document).animate(
-        {scrollTop: $($.attr(this, 'href')).offset.top}, 
-        500
-    );
+$(function() {
+    $('a[href*=\\#]:not([href=\\#])').on('click', function() {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
+        if (target.length) {
+            $('div').animate({scrollTop: target.offset().top}, 1500);
+            return false;
+        }
+    });
 });
-
-// function smoothScrollingTo(target){
-//     $('html,body').animate({scrollTop:$(target).offset().​top}, 500);
-//   }
-// // $('a[href*=\\#]').on('click', function(event){     
-// //     event.preventDefault();
-// //     smoothScrollingTo(this.hash);
-// // });
-// $(document).on('click', function(){
-// smoothScrollingTo(location.hash);
-// });
